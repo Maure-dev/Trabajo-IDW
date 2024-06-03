7d4379ae6c16289f3dd6877a8f841cd219e897b7
Mauro Alejandro Gerardi <mgerardi@leafnoise.io>
2024-06-03 16:13:19 -0300

    React Vite project base with Tailwind CSS and component typing

---
+++ src/entities/entities.ts
@@ -0,0 +1,62 @@
+import { ChangeEvent, FormEvent } from "react";
+
+export type ContactInputFieldType = {
+    type: string;
+    name: string;
+    id: string;
+    placeholder: string;
+}
+
+export type ContactTextAreaType = {
+    name: string;
+    id: string;
+    placeholder: string;
+}
+
+export type ContactSubmitButtonType = {
+    text: string;
+}
+
+export type HomeExperienceCardType = {
+    profile: string;
+    fullName: string;
+    comment: string;
+    isLast: boolean;
+}
+
+export type DestinationCardType = {
+    image: string;
+    description: string;
+}
+
+export type DestinationsSearchBarPropsType = {
+    setSearchTerm: (term: string) => void;
+}
+
+export type DestinationsMainPropsType = {
+    searchTerm: string;
+}
+
+export type AdministrationAccommodationType = {
+    idTipoAlojamiento: number;
+    Descripcion: string;
+}
+
+export type AdministrationFormPropsType = {
+    formState: AdministrationAccommodationType;
+    isEditing: boolean;
+    handleInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
+}
+
+export type AdministrationTablePropsType = {
+    tiposAlojamientos: AdministrationAccommodationType[];
+    handleEdit: (tipoAlojamiento: AdministrationAccommodationType) => void;
+    handleDelete: (id: number) => void;
+}
+
+export type AdministrationRowPropsType = {
+    tipo: AdministrationAccommodationType;
+    handleEdit: (tipoAlojamiento: AdministrationAccommodationType) => void;
+    handleDelete: (id: number) => void;
+}
