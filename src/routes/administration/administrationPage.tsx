9313597ebd8e6973774e2c6f3ca4b9d201d41685
electroninio <javier.gerol@gmail.com>
2024-06-04 18:30:57 -0300

    agrego las rutas

---
+++ src/routes/administration/administrationPage.tsx
@@ -0,0 +1,9 @@
+import AdministrationMainInterface from "../../components/interfaces/administration/AdministrationMainInterface";
+
+export default function AdministrationPage() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-between bg-[#f5f5f5] px-4 lg:px-24 py-4">
+      <AdministrationMainInterface />
+    </div>
+  );
+}
---
+++ src/routes/contact/contactPage.tsx
@@ -0,0 +1,9 @@
+import ContactMainInterface from "../../components/interfaces/contact/ContactMainInterface";
+
+export default function ContactPage() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center bg-[#f5f5f5] px-4 py-4 lg:p-24">
+      <ContactMainInterface />
+    </div>
+  );
+}
